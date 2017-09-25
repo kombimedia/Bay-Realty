6a207030023f856947a34930bfdc1b99b00352d0
kombimedia <cy@kombimedia.nz>
2017-09-25 21:01:34 +1300

    setup product page
    copied index.php to product.php

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
 
-    <?php
-$title = "Bay Realty - Homepage";
-$metaD = "The homepage";
-include 'includes/header.php';
-?>
-
-
-
-    <h1>Hello, worldie!</h1>
-
-    <?php
-  include 'includes/footer.php';
-?>
---
+++ product.php
@@ -0,0 +1,13 @@
+<?php
+$title = "Bay Realty - Product Page";
+$metaD = "Product page";
+include 'includes/header.php';
+?>
+
+
+
+    <h1>Product Page</h1>
+
+    <?php
+  include 'includes/footer.php';
+?>
