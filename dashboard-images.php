d93bfb037b366569e4b265ad9ee1341f4c1382e7
kombimedia <cy@kombimedia.nz>
2017-10-13 10:51:43 +1300

    tidy up

--- dashboard-images.php
+++ dashboard-images.php
@@ -4,16 +4,18 @@ $title = "Images";
 $metaD = "Admin dashboard page, site images";
 include 'includes/dashboard-header.php';
 include 'includes/dashboard-sidebar.php';
+include 'processes/process-populate-images.php';
 ?>
 <h1>Images</h1>
 
+<div><?php if (isset($_SESSION['imageError'])) { echo $_SESSION['imageError']; unset($_SESSION['imageError']); }; ?></div>
 <div><?php if (isset($_SESSION['imageDelSuccess'])) { echo $_SESSION['imageDelSuccess']; unset($_SESSION['imageDelSuccess']); }; ?></div>
 <div><?php if (isset($_SESSION['imageDelError'])) { echo $_SESSION['imageDelError']; unset($_SESSION['imageDelError']); }; ?></div>
 
 <table class="table table-responsive table-striped table-images">
   <thead class="images-head">
     <tr>
-      <th>Featured Image</th>
+      <th>Image</th>
       <th>Image ID</th>
       <th>Listing ID</th>
       <th>Name</th>
@@ -23,35 +25,9 @@ include 'includes/dashboard-sidebar.php';
     </tr>
   </thead>
   <tbody>
-
-<?php
-
-$getData = "SELECT image_id, listing_id, img_name, img_size, img_type
-            FROM images
-            ORDER BY img_name ASC";
-    $result = $mysqli->query($getData);
-    // Check if there are any records to show
-    if ($result->num_rows > 0) {
-        // Loop through data and output each row
-        while($row = $result->fetch_assoc()) {
-?>
-          <tr>
-              <td><img width="250" src="images/uploads/<?php echo $row['img_name'] ?>"></td>
-              <td> <?php echo $row['image_id'] ?> </td>
-              <td> <?php echo $row['listing_id'] ?> </td>
-              <td> <?php echo $row['img_name'] ?> </td>
-              <td> <?php echo $row['img_size']; ?> </td>
-              <td> <?php echo $row['img_type']; ?> </td>
-              <td><a class="delete-image" href="#" onClick="deleteimage('<?php echo $row['image_id']; ?>')"><img src="images/x.png"></a></td>
-          </tr>
-<?php
-        }
-      } else {
-          echo "No property listings to show";
-      }
-?>
-    </tbody>
-  </table>
+    <?php echo $populate_images ?>
+  </tbody>
+</table>
 
 <script language="javascript">
  function deleteimage(dellimage) {
---
+++ processes/process-populate-images.php
@@ -0,0 +1,24 @@
+<?php
+$populate_images = "";
+$stmt = $mysqli->prepare("SELECT image_id, listing_id, img_name, img_size, img_type FROM images ORDER BY img_name ASC");
+$stmt->execute();
+$result = $stmt->get_result();
+if($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+      $arr[] = $row;
+      $populate_images = $populate_images . "
+          <tr>
+              <td><img width='250' src='images/uploads/$row[img_name]'></td>
+              <td>$row[image_id]</td>
+              <td>$row[listing_id]</td>
+              <td>$row[img_name]</td>
+              <td>$row[img_size]</td>
+              <td>$row[img_type]</td>
+              <td><a class='delete-image' href='#' onClick='deleteimage('$row[image_id]')'><img src='images/x.png'></a></td>
+          </tr>";
+        }
+      } else {
+          $_SESSION["imageError"] = "<div class='error-message'>No listing images to show</div>";
+      }
+$stmt->close();
+?>
