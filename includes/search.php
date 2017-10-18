931318ca910745fef27a91ecb34c0dea5f150558
dane <danejbay@yahoo.com>
2017-10-19 08:06:13 +1300

    test

--- includes/search.php
+++ includes/search.php
@@ -1,52 +1,7 @@
 
 
 <?php
-
-include 'processes/process-populate-search-form.php';
-$search_output = "";
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-if(isset($_POST['submit_search'])){
-$search_bar = preg_replace('#[^a-z 0-9?!]#i', '', $_POST['search_bar']);
-$search_bar = "%" . $search_bar . "%";
-
-
-
-$stmt = $mysqli->prepare("SELECT address, price, bed_no, bath_no, property_des, featured_image, title, garage_no FROM properties WHERE MATCH(title) AGAINST  (?) AND categories = ? AND type = ?");
-$stmt->bind_param("sii", $search_bar, $_POST['city'], $_POST['type']);
-$stmt->execute();
-$result = $stmt->get_result();
-
-
-if($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    $arr[] = $row;
-    $search_output = $search_output . "
-
-        <tr>
-        <td><img width='350px' height='200px'  src='images/uploads/$row[featured_image]'></td>
-        <td><h4 style='color: #42b3f4'>$row[title]</h4><p style='color: grey'>$row[address]</p><hr>
-        <p>$row[property_des]</p>
-        <h4>$ $row[price]</h4>
-
-        <p><i class='fa fa-bed' aria-hidden='true'></i>  : $row[bed_no] <i class='fa fa-bath' aria-hidden='true'></i> : $row[bath_no] <i class='fa fa-car' aria-hidden='true'></i> : $row[garage_no]</p>
-
-
-        </tr>";
-
-
-}
-} else{
-  echo "no listins to show";
-}
-$stmt->close();
-
-
-
-}
-$_SESSION['search_output'] = $search_output;
-
-
+include 'processes/process-populate-search-listing.php'
 ?>
 
 
