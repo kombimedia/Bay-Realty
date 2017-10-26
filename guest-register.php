c455f72880e7693d14523b5e1120088365f3809f
dane <danejbay@yahoo.com>
2017-10-26 19:32:44 +1300

    Created about us page and login process

--- guest-register.php
+++ guest-register.php
@@ -2,21 +2,28 @@
 $title = "Bay Realty - Wish List Register Page";
 $metaD = "Wish List login page";
 include 'includes/header.php';
+include 'processes/process-populate-guest-register.php'
 ?>
+  <div><?php if (isset($_SESSION['successMessage'])) { echo $_SESSION['successMessage']; unset($_SESSION['successMessage']); }; ?></div>
+  <div><?php if (isset($_SESSION['errorMessage'])) { echo $_SESSION['errorMessage']; unset($_SESSION['errorMessage']); }; ?></div>
 
 <div class="container-fluid">
     <div class="login-form-box">
       <form class="form-signin" method="post" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
           <h2 class="form-signin-heading mb-4">Wish List Sign Up</h2>
           <label for="guest-first-name" class="sr-only">First Name</label>
-          <input type="text" id="guest-first-name" name="guestFirstName" class="form-control mb-2" placeholder="First Name" required autofocus>
+          <input type="text" id="guest-first-name" name="guestFirstName" class="form-control mb-2" placeholder="First Name" required value="<?php if (isset($_SESSION['storeguestFirstName'])) { echo $_SESSION['storeguestFirstName']; unset($_SESSION['storeguestFirstName']); }; ?>" autofocus>
+           <div><?php if (isset($_SESSION['firstNameError'])) { echo $_SESSION['firstNameError']; unset($_SESSION['firstNameError']); }; ?></div>
           <label for="guest-surname" class="sr-only">Surname</label>
-          <input type="text" id="guest-surname" name="guestSurnameame" class="form-control mb-2" placeholder="Surname" required autofocus>
+          <input type="text" id="guest-surname" name="guestSurname" class="form-control mb-2" placeholder="Surname" required value="<?php if (isset($_SESSION['storeguestSurname'])) { echo $_SESSION['storeguestSurname']; unset($_SESSION['storeguestSurname']); }; ?>" autofocus>
+          <div><?php if (isset($_SESSION['surnameError'])) { echo $_SESSION['surnameError']; unset($_SESSION['surnameError']); }; ?></div>
           <label for="inputEmail" class="sr-only">Email address</label>
-          <input type="email" id="inputEmail" name="guestEmail" class="form-control mb-2" placeholder="Email address" required autofocus>
+          <input type="email" id="inputEmail" name="guestEmail" class="form-control mb-2" placeholder="Email address" required value="<?php if (isset($_SESSION['storeguestEmail'])) { echo $_SESSION['storeguestEmail']; unset($_SESSION['storeguestEmail']); }; ?>" autofocus>
+          <div><?php if (isset($_SESSION['emailError'])) { echo $_SESSION['emailError']; unset($_SESSION['emailError']); }; ?></div>
           <label for="inputPassword" class="sr-only">Password</label>
-          <input type="password" id="inputPassword" name="guestPassword" class="form-control mb-3" placeholder="Password" required>
-          <button class="btn btn-md btn-block" type="submit">Sign up</button>
+          <input type="password" id="inputPassword" name="guestPassword" class="form-control mb-3" placeholder="Password" value="<?php if (isset($_SESSION['storeguestPassword'])) { echo $_SESSION['storeguestPassword']; unset($_SESSION['storeguestPassword']); }; ?>" required>
+          <div><?php if (isset($_SESSION['passwordError'])) { echo $_SESSION['passwordError']; unset($_SESSION['passwordError']); }; ?></div>
+          <button class="btn btn-md btn-block" type="submit" name="submit" value="Create User">Sign up</button>
         </form>
         <p>Already registered for Wish List? <a class="form-link" href="guest-login">Log in!</a></p>
     </div>
---
+++ processes/process-populate-guest-register.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+include 'process-functions-validation.php';
+
+// Save form data to global variables to repopulate form if submit fails
+// $_SESSION['storeguestFirstName'] = $_POST['guestFirstName'];
+// $_SESSION['storeguestSurname'] = $_POST['guestSurname'];
+// $_SESSION['storeguestEmail'] = $_POST['guestEmail'];
+// $_SESSION['storeguestPassword'] = $_POST['guestPassword'];
+
+// Validate input fields
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // validate first name
+    $validName = true;
+    if (!validate_first_name ($_POST["guestFirstName"])) {
+        $validName = false;
+    }
+
+    // Validate surname
+    $validSName = true;
+    if (!validate_surname ($_POST["guestSurname"])) {
+        $validSName = false;
+    }
+
+    // Validate email address
+    $validEmail = true;
+    if (!validate_email ($_POST["guestEmail"])) {
+        $validEmail = false;
+    }
+
+    // // Validate password
+    $validPassword = true;
+    if (!validate_password ($_POST["guestPassword"])) {
+        $validPassword = false;
+    }
+    $_POST["guestPassword"] = md5($_POST["guestPassword"]);
+
+   
+}
+
+// If all validation passes set validForm variable to true
+$validForm = $validName && $validSName && $validEmail && $validPassword;
+
+// Only connect to database if form passes validation
+if ($validForm) {
+    // Create db connection
+    include '../includes/db-connect.php';
+    // Check whether email address has already been registered in the database
+    $stmt = $mysqli->prepare("SELECT email FROM users WHERE email = ?");
+    $stmt->bind_param("s", $_POST["guestEmail"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows == 0) {
+        $guest_user_role = 3;
+        // if email has not previously been registered, add new user details to database
+        $stmt1 = $mysqli->prepare("INSERT INTO users (first_name, surname, email, role, password) VALUES (?, ?, ?, ?, ?)");
+        $stmt1->bind_param("sssis", $_POST['guestFirstName'], $_POST['guestSurname'], $_POST['guestEmail'], $guest_user_role, $_POST['guestPassword']);
+        if ($stmt1->execute()) {
+            header('location: ../wishlist.php');
+        } else {
+          // if insert is unsuccessful throw error
+           header('location: ../guest-register.php');
+           $_SESSION["errorMessage"] = "<div class='error-message'>An error has occurred: " . $stmt->errno . " " . $stmt->error. "</div>";
+           }
+           $stmt1->close();
+    } else {
+        // if email already exists in db, throw error
+        header('location: ../guest-register.php');
+        $_SESSION["errorMessage"] = "<div class='error-message'>This email address is already registered</div>";
+        exit;
+    }
+    $stmt->close();
+} else {
+    // if form does not pass validation redirect back to registration page
+    header('Location: ../guest-register.php');
+    exit;
+  }
+// Get the just created listing_id and save to a variable
+$new_user_id = mysqli_insert_id($mysqli);
+
+// if user is successfully created go back to dashboard 'add user' page and print success message
+$_SESSION["successMessage"] = "<div class='success-message'>New User with ID: " . $new_user_id . " successfully created.</div>";
+header('location: ../wishlist.php');
