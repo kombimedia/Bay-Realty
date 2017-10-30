ddda1f2575ee02c398fb330aaf91bcb7725e10c5
dane <danejbay@yahoo.com>
2017-10-30 13:07:37 +1300

    wishlist

--- processes/process-wishlist-button.php
+++ processes/process-wishlist-button.php
@@ -1,47 +1,38 @@
-<?php 
+ <?php
 include "../includes/db-connect.php";
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 $listing_id = $_GET['listing_id'];
-$listin_id = [];
 $user_id = $_SESSION['user_id'];
 
-if (isset($_SESSION['guestUserName'])) { 
+$wislist_array = array();
 
-$stmt1 = $mysqli->prepare("SELECT my_wishlist FROM users WHERE user_id = ?");
-$stmt1->bind_param("i",  $user_id);
-if(!$stmt1->execute()) {
-  echo "select failed ("$stmt1->errno . ") " . $stmt1->error;
+if (isset($_SESSION['guestUserName'])) {
+
+    $stmt = $mysqli->prepare("SELECT my_wishlist FROM users WHERE user_id = ?");
+    $stmt->bind_param("i", $user_id);
+    if (!$stmt->execute()) {
+      //echo "Select failed (" . $stmt->errno . ") " . $stmt->error; // change to a session error on page
+      exit;
+    }
+    $result = $stmt->get_result();
+    if($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        $wishlist_array = array($row['my_wishlist']);
+      }
+    }
+    $stmt->close();
+
+    array_push($wishlist_array, $listing_id);
+    $wishlist_array = implode(",",$wishlist_array);
+
+    $stmt = $mysqli->prepare("UPDATE users SET my_wishlist = ? WHERE user_id = ?");
+    $stmt->bind_param("si", $wishlist_array, $user_id);
+    if (!$stmt->execute()) {
+      //echo "Insert failed (" . $stmt->errno . ") " . $stmt->error; // change to a session error on page
+      exit;
+    }
+    $stmt->close();
+    header ('location: ../wishlist');
 }
-$result = $stmt1->get_result();
-
-
-if($result->num_rows > 0) {
-$wislist_array = $row['my_wishlist']; 
-} else {
-  echo "no user row selected";
-}
-
-$stmt1->close();
-array_push($wishlist_array, $listing_id); 
-
-
-  
-$stmt = $mysqli->prepare("UPDATE users SET my_wishlist = ? WHERE user_id = ?");
-$stmt->bind_param("ii", $wishlist_array, $user_id);
-if(!$stmt->execute()) {
-  echo "update failed ("$stmt->errno . ") " . $stmt->error;
-}
-
-$stmt->close();
-
-} else {
-  echo "login error";
-}
-
-echo "script executed";
-
-  // else{
-  //  header('location: ../guest-login');
-  // }
