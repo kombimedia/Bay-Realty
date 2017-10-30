4f29286af35f4640a69ff044bb0d4233d8ba9997
kombi Media <cy@kombimedia.nz>
2017-10-30 13:09:07 +1300

    merge

--- processes/process-wishlist-button.php
+++ processes/process-wishlist-button.php
@@ -1,47 +1,24 @@
-<?php 
-include "../includes/db-connect.php";
+<?php
+
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-$listing_id = $_GET['listing_id'];
-$listin_id = [];
-$user_id = $_SESSION['user_id'];
+if (isset($_SESSION['guestUserName'])) {
 
-if (isset($_SESSION['guestUserName'])) { 
-
-$stmt1 = $mysqli->prepare("SELECT my_wishlist FROM users WHERE user_id = ?");
-$stmt1->bind_param("i",  $user_id);
-if(!$stmt1->execute()) {
-  echo "select failed ("$stmt1->errno . ") " . $stmt1->error;
-}
-$result = $stmt1->get_result();
+	$listing_id = $_GET['listing_id'];
 
 
-if($result->num_rows > 0) {
-$wislist_array = $row['my_wishlist']; 
-} else {
-  echo "no user row selected";
-}
-
-$stmt1->close();
-array_push($wishlist_array, $listing_id); 
 
 
-  
-$stmt = $mysqli->prepare("UPDATE users SET my_wishlist = ? WHERE user_id = ?");
-$stmt->bind_param("ii", $wishlist_array, $user_id);
-if(!$stmt->execute()) {
-  echo "update failed ("$stmt->errno . ") " . $stmt->error;
-}
+  $stmt1 = $mysqli->prepare("INSERT INTO agents (wishlist) VALUES (?) WHERE ");
+        $stmt1->bind_param("sssssis", $_POST['listing_id']);
 
-$stmt->close();
+ $stmt->execute();
+ $result = $stmt->get_result();
+ if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      // Loop through each row and save the array to a variable
+      $first_name = $row['first_name'];
+   }
 
-} else {
-  echo "login error";
-}
-
-echo "script executed";
-
-  // else{
-  //  header('location: ../guest-login');
-  // }
+	else{
+		header('location: ../guest-login');
+	};
