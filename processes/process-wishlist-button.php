fa155bc892b8a9f7d5a3a4e1701583032fd407a8
kombi Media <cy@kombimedia.nz>
2017-10-31 14:37:01 +1300

    wishlist button
    Dane and I worked together to create wishlist db and create insert query that checks for duplicates and redirects to login page if user is not logged in

--- processes/process-wishlist-button.php
+++ processes/process-wishlist-button.php
@@ -1,41 +1,95 @@
 <?php
-
 session_start();
+// Check if user is logged in
+if (!isset($_SESSION['guestUserName'])) {
+    header('location: ../guest-login');
+    exit;
+}
+
 include "../includes/db-connect.php";
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 $listing_id = $_GET['listing_id'];
 $user_id = $_SESSION['user_id'];
+$ws_listing_id = true;
 
-$wislist_array = array();
-
-if (isset($_SESSION['guestUserName'])) {
-
-    $stmt = $mysqli->prepare("SELECT my_wishlist FROM users WHERE user_id = ?");
-    $stmt->bind_param("i", $user_id);
-    if (!$stmt->execute()) {
-      //echo "Select failed (" . $stmt->errno . ") " . $stmt->error; // change to a session error on page
-      exit;
-    }
-    $result = $stmt->get_result();
-    if($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-        $wishlist_array = array($row['my_wishlist']);
+$stmt = $mysqli->prepare("SELECT user_id, listing_id FROM wishlist WHERE user_id = ?");
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+if($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+      if ($listing_id != $row['listing_id']) {
+          $ws_listing_id = true;
+      } else {
+        $ws_listing_id = false;
+        // echo "this listing is already in your wishlist";
+        $_SESSION['wlError'] = "Listing with ID: " . $listing_id . " is already saved to your wishlist";
+        header('location: ../wishlist');
+        exit;
       }
-    }
-    $stmt->close();
-
-    array_push($wishlist_array, $listing_id);
-    $wishlist_array = implode(",",$wishlist_array);
-
-    $stmt = $mysqli->prepare("UPDATE users SET my_wishlist = ? WHERE user_id = ?");
-    $stmt->bind_param("si", $wishlist_array, $user_id);
-    if (!$stmt->execute()) {
-      //echo "Insert failed (" . $stmt->errno . ") " . $stmt->error; // change to a session error on page
-      exit;
-    }
-    $stmt->close();
-    header ('location: ../wishlist');
-} else {
-  header('location: ../guest-login');
+  }
 }
+$stmt->close();
+
+if ($ws_listing_id) {
+    $stmt = $mysqli->prepare("INSERT INTO wishlist (user_id, listing_id) VALUES (?, ?)");
+    $stmt->bind_param("ii", $user_id, $listing_id);
+    $stmt->execute();
+    $stmt->close();
+}
+$_SESSION['wlSuccess'] = "Listing with ID: " . $listing_id . " successfully added to your wishlist";
+header('location: ../wishlist');
+
+// print_r ("listing id" . $ws_listing_id);
+
+// if ($listing_id != $ws_listing_id) {
+//         $stmt = $mysqli->prepare("INSERT INTO wishlist (user_id, listing_id) VALUES (?, ?)");
+//         $stmt->bind_param("ii", $user_id, $listing_id);
+//         $stmt->execute();
+//     } else {
+//       //echo "this listing is already in your wishlist";
+//       //header('location: ../wishlist');
+//     }
+// $stmt->close();
+
+
+// session_start();
+// include "../includes/db-connect.php";
+// error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// $listing_id = $_GET['listing_id'];
+// $user_id = $_SESSION['user_id'];
+
+// $wislist_array = array();
+
+// if (isset($_SESSION['guestUserName'])) {
+
+//     $stmt = $mysqli->prepare("SELECT my_wishlist FROM users WHERE user_id = ?");
+//     $stmt->bind_param("i", $user_id);
+//     if (!$stmt->execute()) {
+//       //echo "Select failed (" . $stmt->errno . ") " . $stmt->error; // change to a session error on page
+//       exit;
+//     }
+//     $result = $stmt->get_result();
+//     if($result->num_rows > 0) {
+//       while($row = $result->fetch_assoc()) {
+//         $wishlist_array = array($row['my_wishlist']);
+//       }
+//     }
+//     $stmt->close();
+
+//     array_push($wishlist_array, $listing_id);
+//     $wishlist_array = implode(",",$wishlist_array);
+
+//     $stmt = $mysqli->prepare("UPDATE users SET my_wishlist = ? WHERE user_id = ?");
+//     $stmt->bind_param("si", $wishlist_array, $user_id);
+//     if (!$stmt->execute()) {
+//       //echo "Insert failed (" . $stmt->errno . ") " . $stmt->error; // change to a session error on page
+//       exit;
+//     }
+//     $stmt->close();
+//     header ('location: ../wishlist');
+// } else {
+//   header('location: ../guest-login');
+// }
--- wishlist.php
+++ wishlist.php
@@ -9,6 +9,9 @@ include 'includes/header.php';
 include 'includes/search.php';
 include 'processes/process-populate-wishlist.php';
 ?>
+
+  <div><?php if (isset($_SESSION['wlSuccess'])) { echo $_SESSION['wlSuccess']; unset($_SESSION['wlSuccess']); }; ?></div>
+  <div><?php if (isset($_SESSION['wlError'])) { echo $_SESSION['wlError']; unset($_SESSION['wlError']); }; ?></div>
 	<table>
 	<? echo $display_wishlist ?>
  	</table>
